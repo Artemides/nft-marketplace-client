f218ca1486d8ca39e6d7a796354ee61587de49c0
Artemides <edmpulasky@gmail.com>
2023-07-05 22:01:23 -0500

    set up next auth

---
+++ src/app/providers.tsx
@@ -0,0 +1,6 @@
+import { SessionProvider } from "next-auth/react";
+import { PropsWithChildren } from "react";
+
+export const NextAuthProvider = ({ children }: PropsWithChildren) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
