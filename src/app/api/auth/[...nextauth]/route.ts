81eaf50e04f0a46dbb049cec3a3205aa7cae5477
Artemides <edmpulasky@gmail.com>
2023-07-05 22:11:34 -0500

    set up moralis and next auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import { MoralisNextAuthProvider } from "@moralisweb3/next";
+import NextAuth from "next-auth/next";
+
+export default NextAuth({
+  providers: [MoralisNextAuthProvider()],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }) {
+      if (token.name && session.user) session.user.name = token.name;
+
+      return session;
+    },
+  },
+});
